411b1ceb410d0618f10182f5cf5d17eaecc34a0a
Dileep0807 <kumar.dileep0807@gmail.com>
2025-01-19 16:25:27 +0530

    screenshit test case

---
+++ src/test/java/Listeners/ListenersMethod.java
@@ -0,0 +1,46 @@
+package Listeners;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class ListenersMethod extends Base implements ITestListener {
+	
+	    @Override
+	    public void onTestStart(ITestResult result) {
+	        // Code to execute when a test starts
+	    }
+
+	    @Override
+	    public void onTestSuccess(ITestResult result) {
+	        // Code to execute when a test passes
+	    }
+
+	    @Override
+	    public void onTestFailure(ITestResult result) {
+	       
+	    	System.out.print("Failed Test Cases:");
+	    	failed(result.getMethod().getMethodName());
+	    }
+
+	    @Override
+	    public void onTestSkipped(ITestResult result) {
+	        // Code to execute when a test is skipped
+	    }
+
+	    @Override
+	    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+	        // Code to execute when a test fails but within success percentage
+	    }
+
+	    @Override
+	    public void onStart(ITestContext context) {
+	        // Code to execute before any test method starts in the context
+	    }
+
+	    @Override
+	    public void onFinish(ITestContext context) {
+	        // Code to execute after all the test methods have finished
+	    }
+
+}
---
+++ src/test/java/Listeners/ScreenshotTest.java
@@ -0,0 +1,37 @@
+package Listeners;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(ListenersMethod.class)
+
+public class ScreenshotTest extends Base {
+    
+	
+	
+	@BeforeMethod
+	public void setup() 
+	{
+    	 initialization();
+	
+	}
+     @AfterMethod
+     public void tearDown() 
+     {
+    	 driver.quit();
+     }
+     @Test
+     public void TakeScreenShot1()
+     {
+    	Assert.assertEquals(false, true); 
+     }
+     
+     @Test
+     public void TakeScreenShot2()
+     {
+    	Assert.assertEquals(false, true); 
+     }
+}
